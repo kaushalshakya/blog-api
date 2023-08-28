e6b5aa845f6f1675b9dc83781a7357c4ce900ad9
kaushalshakya <kshakya101@gmail.com>
2023-08-28 15:49:04 +0545

    user registration complete

--- models/auth/registerModel.js
+++ models/auth/registerModel.js
@@ -5,14 +5,14 @@ var qb;
 
 const registerUserModel = asyncHandler(async(data) =>{
     qb = await pool.get_connection();
-    const response = await qb.insert('Users', {
+    const response = await qb.insert('users', {
         first_name: data.first_name,
         last_name: data.last_name,
         email: data.email,
         password: data.password,
         image: data.image
     })
-    await qb.disconnect();
+    await qb.release();
     return response;
 })
 
--- routes/auth/registerRoute.js
+++ routes/auth/registerRoute.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads')
+      cb(null, 'uploads/profile-img');
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
